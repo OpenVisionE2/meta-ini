6bef22edd184995281c9b8ab532272c3feb8174c
persianpros <persianpros@yahoo.com>
2021-02-20 13:48:39 +0330

    Cleanup sh files.

--- recipes-bsp/extra/ini-ethwol/ethwol.sh
+++ recipes-bsp/extra/ini-ethwol/ethwol.sh
@@ -11,10 +11,10 @@ PATH=/sbin:/bin:/usr/sbin:/usr/bin
 grep -q 'enable' /proc/stb/fp/wol
 if [ $? -eq 0 ]
 then
-	echo [WOL] activate WakeOnLAN at device eth0
+	echo "[WOL] activate WakeOnLAN at device eth0"
 	ethtool -s eth0 wol g
 else
-	echo [WOL] WakeOnLAN is not enabled
+	echo "[WOL] WakeOnLAN is not enabled"
 fi
 
 : exit 0
